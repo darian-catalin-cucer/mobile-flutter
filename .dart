634a29c32438ab85f5bdbe77339e6b7c2be2f0e9
Darian-Cătălin Cucer <cucerdariancatalin@gmail.com>
2023-03-09 21:41:06 +0200

    Create .dart

---
+++ .dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello, Flutter!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello, Flutter!'),
+        ),
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
